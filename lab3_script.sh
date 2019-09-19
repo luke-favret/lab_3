4351d0ce4ef9eb64292c353fb6d742fd49b84585
luke-favret <luke.favret@colorado.edu>
2019-09-18 18:55:20 -0600

    Implemented my shell script for the answers to the questions.

---
+++ lab3_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Authors : Luke Favret
+# Date: 9/18/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+echo "Enter a file name: "
+read fileName
+echo "Enter a regular expression: "
+read regexIn
+grep $regexIn $fileName
+echo "The file has this many phone numbers: "
+grep -c '[0-9]\{3\}-[0-9]\{4\}' regex_practice.txt
+grep "303-" regex_practice.txt
+grep "@geocities.com" regex_practice.txt >> email_results.txt
+git add email_results.txt
+git commit -m "Automatic commit from shell script of email results"
+echo "Script done"
